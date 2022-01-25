a70ed2b916fe660dcd4e4068361d9967d11443bd
lorenzo <lorenzo.pisaneschi1@stud.unifi.it>
2022-01-25 12:24:12 +0100

    Improve color-category mappings

--- layout_transformer/main.py
+++ layout_transformer/main.py
@@ -7,42 +7,54 @@ from model import GPT, GPTConfig
 from trainer import Trainer, TrainerConfig
 from utils import set_seed
 import json
-from pathlib import Path
-
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser('Layout Transformer')
+    parser = argparse.ArgumentParser("Layout Transformer")
     parser.add_argument("--exp", default="layout", help="experiment name")
     parser.add_argument("--log_dir", default="./logs", help="/path/to/logs/dir")
 
     # MNIST options
     parser.add_argument("--data_dir", default=None, help="/path/to/mnist/data")
-    parser.add_argument("--threshold", type=int, default=16, help="threshold for grayscale values")
+    parser.add_argument(
+        "--threshold", type=int, default=16, help="threshold for grayscale values"
+    )
 
     # COCO/PubLayNet options
-    parser.add_argument("--train_json", default="./instances_train.json", help="/path/to/train/json")
-    parser.add_argument("--val_json", default="./instances_val.json", help="/path/to/val/json")
+    parser.add_argument(
+        "--train_json", default="./instances_train.json", help="/path/to/train/json"
+    )
+    parser.add_argument(
+        "--val_json", default="./instances_val.json", help="/path/to/val/json"
+    )
 
     # Layout options
     parser.add_argument("--max_length", type=int, default=128, help="batch size")
-    parser.add_argument('--precision', default=8, type=int)
-    parser.add_argument('--element_order', default='raster')
-    parser.add_argument('--attribute_order', default='cxywh')
+    parser.add_argument("--precision", default=8, type=int)
+    parser.add_argument("--element_order", default="raster")
+    parser.add_argument("--attribute_order", default="cxywh")
 
     # Architecture/training options
     parser.add_argument("--seed", type=int, default=42, help="random seed")
-    parser.add_argument("--epochs", type=int, default=20, help="number of epochs")
+    parser.add_argument("--epochs", type=int, default=25, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=64, help="batch size")
     parser.add_argument("--lr", type=float, default=4.5e-06, help="learning rate")
-    parser.add_argument('--n_layer', default=6, type=int)
-    parser.add_argument('--n_embd', default=512, type=int)
-    parser.add_argument('--n_head', default=8, type=int)
+    parser.add_argument("--n_layer", default=6, type=int)
+    parser.add_argument("--n_embd", default=512, type=int)
+    parser.add_argument("--n_head", default=8, type=int)
     # parser.add_argument('--evaluate', action='store_true', help="evaluate only")
-    parser.add_argument('--lr_decay', action='store_true', help="use learning rate decay")
-    parser.add_argument('--warmup_iters', type=int, default=0, help="linear lr warmup iters")
-    parser.add_argument('--final_iters', type=int, default=0, help="cosine lr final iters")
-    parser.add_argument('--sample_every', type=int, default=1, help="sample every epoch")
+    parser.add_argument(
+        "--lr_decay", action="store_true", help="use learning rate decay"
+    )
+    parser.add_argument(
+        "--warmup_iters", type=int, default=0, help="linear lr warmup iters"
+    )
+    parser.add_argument(
+        "--final_iters", type=int, default=0, help="cosine lr final iters"
+    )
+    parser.add_argument(
+        "--sample_every", type=int, default=1, help="sample every epoch"
+    )
 
     args = parser.parse_args()
 
@@ -60,33 +72,49 @@ if __name__ == "__main__":
     # MNIST Testing
     if args.data_dir is not None:
         train_dataset = MNISTLayout(args.log_dir, train=True, threshold=args.threshold)
-        valid_dataset = MNISTLayout(args.log_dir, train=False, threshold=args.threshold,
-                                    max_length=train_dataset.max_length)
+        valid_dataset = MNISTLayout(
+            args.log_dir,
+            train=False,
+            threshold=args.threshold,
+            max_length=train_dataset.max_length,
+        )
     # COCO and PubLayNet
     else:
         train_dataset = JSONLayout(args.train_json)
         valid_dataset = JSONLayout(args.val_json, max_length=train_dataset.max_length)
 
-    mconf = GPTConfig(train_dataset.vocab_size, train_dataset.max_length,
-                      n_layer=args.n_layer, n_head=args.n_head, n_embd=args.n_embd)  # a GPT-1
+    mconf = GPTConfig(
+        train_dataset.vocab_size,
+        train_dataset.max_length,
+        n_layer=args.n_layer,
+        n_head=args.n_head,
+        n_embd=args.n_embd,
+    )  # a GPT-1
     model = GPT(mconf)
-    tconf = TrainerConfig(max_epochs=args.epochs,
-                          batch_size=args.batch_size,
-                          lr_decay=args.lr_decay,
-                          learning_rate=args.lr * args.batch_size,
-                          warmup_iters=args.warmup_iters,
-                          final_iters=args.final_iters,
-                          ckpt_dir=ckpt_dir,
-                          samples_dir=samples_dir,
-                          sample_every=args.sample_every)
-    
-    if not Path("colors.json").is_file():
-        with open("colors.json", "w") as f:
-            categories = train_dataset.categories
-            for idx, k in enumerate(categories.keys()):
-                r, g, b = train_dataset.colors[idx]
-                categories[k]["color"] = rgb2hex(r, g, b)
-            json.dump(categories, f)
-        
+    tconf = TrainerConfig(
+        max_epochs=args.epochs,
+        batch_size=args.batch_size,
+        lr_decay=args.lr_decay,
+        learning_rate=args.lr * args.batch_size,
+        warmup_iters=args.warmup_iters,
+        final_iters=args.final_iters,
+        ckpt_dir=ckpt_dir,
+        samples_dir=samples_dir,
+        sample_every=args.sample_every,
+    )
+
+    with open("colors.json", "w") as f:
+        categories = train_dataset.categories
+        colors = {}
+        for idx, k in enumerate(categories.keys()):
+            r, g, b = train_dataset.colors[idx]
+            color_hex = rgb2hex(r, g, b)
+            colors[color_hex] = {
+                "category": categories[k]["name"],
+                "color_rgb": (r, g, b),
+            }
+        train_dataset.colors_categories_map = colors
+        json.dump(colors, f)
+
     trainer = Trainer(model, train_dataset, valid_dataset, tconf, args)
     trainer.train()
